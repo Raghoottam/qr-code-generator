df6062e9502f55d944a8219438b77b9f8fde5259
Raghoottam Katti <raghukatti0@gmail.com>
2022-03-19 22:25:45 +0530

    Resolving build errors

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Homepage from './components/Homepage';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 
 function App() {
   return (
     <div className='App'>
       <Navbar/>
       <Homepage/>
-      //<Footer/> Commenting to remove footer until UI is designed
+      {/*<Footer/> Commenting to remove footer until UI is designed*/}
     </div>
   );
 }
