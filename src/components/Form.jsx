ee7d9c8df7b4a59ec379990e9cd403ec95f6c2d0
Raghoottam Katti <raghukatti0@gmail.com>
2022-03-20 12:25:49 +0530

    Css changes and added under development toast

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,10 +6,10 @@ import 'react-toastify/dist/ReactToastify.css';
 toast.configure()
 
 export default function Form(props) {
-    const [level, setLevel] = useState('H')
-    const [value, setValue] = useState('Welcome')
-    const [size, setSize] = useState(250)
-    const [title, setTitle] = useState('Test Title')
+    const [level, setLevel] = useState('')
+    const [value, setValue] = useState('')
+    const [size, setSize] = useState()
+    const [title, setTitle] = useState('')
 
     const onValueChange = (e) => {
         setValue(e.target.value)
@@ -25,9 +25,8 @@ export default function Form(props) {
         if (e.target.value < 350) {
             setSize(e.target.value)
             props.onSizeChange(e.target.value)
-        } else
-        {
-            toast.error('Size should be less than 350', {autoClose:3000})
+        } else {
+            toast.error('Size should be less than 350', { autoClose: 3000 })
         }
     }
 
@@ -42,31 +41,70 @@ export default function Form(props) {
 
     return (
         <>
-        <form className='container Form'>
-            <div className='p-1 row'>
-                <label className='col-3 mx-2'>Value</label>
-                <input type="text" className="col-8 field" value={value} onChange={(e) => { onValueChange(e) }} />
-            </div>
-            <div className='p-1 row'>
-                <label className='col-3 mx-2'>Size</label>
-                <input type="number" className="col-8 field" max={340} value={size} onChange={(e) => onSizeChange(e)} />
-            </div>
-            <div className='p-1 row'>
-                <label className='col-3 mx-2'>Level</label>
-                <select className="form-select-md sm-3 col-8 field" aria-label=".form-select-sm example" selected={level} onChange={(e) => onLevelChange(e)}>
-                    <option value="L">Low</option>
-                    <option value="M">Medium</option>
-                    <option value="Q">Quartile</option>
-                    <option value="H">High</option>
-                </select>
-            </div>
-            <div className='p-1 row'>
-                <label className='col-3 mx-2'>Title</label>
-                <input type="text" className="col-8 field" value={title} onChange={(e) => onTitleChange(e)} />
-            </div>
-            <Button onClick={() => handleOnSubmit()}>Submit</Button>
-        </form>
-        <ToastContainer/>
+            {/* <form className='container Form'>
+                <div className='p-1 row'>
+                    <label className='col-3 mx-2'>Value</label>
+                    <input type="text" className="col-8 field" value={value} onChange={(e) => { onValueChange(e) }} />
+                </div>
+                <div className='p-1 row'>
+                    <label className='col-3 mx-2'>Size</label>
+                    <input type="number" className="col-8 field" max={340} value={size} onChange={(e) => onSizeChange(e)} />
+                </div>
+                <div className='p-1 row'>
+                    <label className='col-3 mx-2'>Level</label>
+                    <select className="form-select-md sm-3 col-8 field" aria-label=".form-select-sm example" selected={level} onChange={(e) => onLevelChange(e)}>
+                        <option value="L">Low</option>
+                        <option value="M">Medium</option>
+                        <option value="Q">Quartile</option>
+                        <option value="H">High</option>
+                    </select>
+                </div>
+                <div className='p-1 row'>
+                    <label className='col-3 mx-2'>Title</label>
+                    <input type="text" className="col-8 field" value={title} onChange={(e) => onTitleChange(e)} />
+                </div>
+                <Button onClick={() => handleOnSubmit()}>Submit</Button>
+            </form> */}
+            <ToastContainer />
+            <center>
+                <div className='title'>
+                    Create Your QR Code
+                </div>
+                <div class="floating-form">
+                    <div className='padding'></div>
+                    <div class="floating-label">
+                        <textarea class="floating-input floating-textarea" placeholder=" " value={value} onChange={(e) => { onValueChange(e) }}></textarea>
+                        <span class="highlight"></span>
+                        <label>Value</label>
+                    </div>
+
+                    <div class="floating-label">
+                        <input class="floating-input" type="number" placeholder=" " value={size} onChange={(e) => onSizeChange(e)} />
+                        <span class="highlight"></span>
+                        <label>Size</label>
+                    </div>
+                    <div class="floating-label">
+                        <select class="floating-select" selected={level} onSelect={(e) => onLevelChange(e)}>
+                            <option value="" selected disabled>Select Level</option>
+                            <option value="L">Low</option>
+                            <option value="M">Medium</option>
+                            <option value="Q">Quartile</option>
+                            <option value="H">High</option>
+                        </select>
+                        <span class="highlight"></span>
+                    </div>
+
+                    <div class="floating-label">
+                        <input class="floating-input" type="text" placeholder=" " value={title} onChange={(e) => onTitleChange(e)} />
+                        <span class="highlight"></span>
+                        <label>Title</label>
+                    </div>
+                    <div class="floating-label">
+                        <button className='btn btn-outline-success' onClick={() => handleOnSubmit()}>Submit</button>
+                    </div>
+                    <div className='padding'></div>
+                </div>
+            </center>
         </>
     )
 }
