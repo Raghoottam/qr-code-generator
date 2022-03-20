50cb28847d3e1488f381856258a5958f8f4b5e6a
Raghoottam Katti <raghukatti0@gmail.com>
2022-03-20 12:31:12 +0530

    Resolving build errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,6 @@ import'../Style/Footer.css'
 
 export default function Footer() {
   return (
-    <div class='floating-credit'>Created by : <a href='https://www.google.com' target='_blank'>Raghoottam Katti</a></div>
+    <div class='floating-credit'>Created by : <a href='https://www.google.com' rel='noopener'>Raghoottam Katti</a></div>
   )
 }
