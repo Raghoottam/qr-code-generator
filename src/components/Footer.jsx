367f22a8faf1a1eef00e89f3b09eb296e1c73999
Raghoottam Katti <raghukatti0@gmail.com>
2022-03-23 23:01:53 +0530

    Code changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,6 @@ import'../Style/Footer.css'
 
 export default function Footer() {
   return (
-    <div className='floating-credit'>Created by : <a href='https://www.google.com' rel='noopener'>Raghoottam Katti</a></div>
+    <div className='floating-credit'>Developed by : <a href='https://www.google.com' rel='noopener'>Raghoottam Katti</a></div>
   )
 }
